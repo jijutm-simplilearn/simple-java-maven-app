1d8ea875b43e0b62e83e9f756e755efeb933bff4
jijutm-simplilearn <mail@jijutm.com>
2021-03-10 21:46:01 +0530

    Success Build

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 
     private final String message = "Hello World!";
 
-    private final Strinng checkit = 'This will break';
+    //private final Strinng checkit = 'This will break';
 
     public App() {}
 
